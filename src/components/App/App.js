c5ff9625e62f474561c88a84ca2cbba27c698335
Danil Volkov <volchara287@gmail.com>
2024-06-06 16:23:58 +0400

    fix: обратно возвращает директорию build в gitignore

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Trail Protection
+          Cooking Trail Protection 🍳
         </p>
         <a
           className="App-link"
