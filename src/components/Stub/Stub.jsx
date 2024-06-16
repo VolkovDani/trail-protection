43249d6dba011ecb092c94df46537e50892038f1
Danil Volkov <volchara287@gmail.com>
2024-06-16 14:23:09 +0400

    feat: изменяет название App на Stub
    Так как это заглушка, называю Stub

---
+++ src/components/Stub/Stub.jsx
@@ -0,0 +1,28 @@
+import { useTranslation } from 'react-i18next';
+import brand from '../../assets/brand.svg';
+import './Stub.css';
+
+const Stub = () => {
+  const { t } = useTranslation('Stub');
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={brand} className="App-logo" alt="brand" />
+        <p>
+          {t('testcooking')}
+          {/* Cooking Trail Protection 🍳 */}
+        </p>
+        <a
+          className="App-link"
+          href={t('links.vk')}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {t('vkGroup')}
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default Stub;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App/App.js';
-import reportWebVitals from './reportWebVitals';
+import Stub from './components/Stub/Stub.jsx';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+        <Stub />
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
