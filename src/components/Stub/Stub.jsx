7edfa6e3dafaae52ea70a589d88f0daf4c928167
Danil Volkov <volchara287@gmail.com>
2024-06-16 15:46:03 +0400

    add: спиннер для Stub

--- src/components/Stub/Stub.jsx
+++ src/components/Stub/Stub.jsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'react-i18next';
+import Spinner from 'react-bootstrap/Spinner';
 import brand from '../../assets/brand.svg';
 import './Stub.css';
 
@@ -10,6 +11,8 @@ const Stub = () => {
         <img src={brand} className="App-logo" alt="brand" />
         <p>
           {t('testcooking')}
+          <span>  </span>
+          <Spinner animation="border" variant="light" />
         </p>
         <a
           className="App-link"
--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,9 +1,16 @@
 export default {
   Stub: {
-    testcooking: 'Cooking Trail Protection🍳',
+    testcooking: '🍳Cooking Trail Protection',
     vkGroup: 'Группа ВКонтакте',
     links: {
       vk: 'https://vk.com/trailprotection',
     },
+  },
+  Header: {
+    brandname: 'Trail Protection',
+    home: 'Главная',
+    customers: 'Покупатели',
+    about: 'О нас',
+    contact: 'Контакты',
   },
 };
