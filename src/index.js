a5dda2126d9ee84852cc0dd90c07febfd85b4d6a
Danil Volkov <volchara287@gmail.com>
2024-06-16 14:24:41 +0400

    feat: создаёт словарь для русского сайта

--- src/index.js
+++ src/index.js
@@ -1,11 +1,39 @@
+/* eslint-disable functional/no-expression-statements */
+/* eslint-disable comma-dangle */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import i18next from 'i18next';
+import { I18nextProvider, initReactI18next } from 'react-i18next';
 import './index.css';
+import './index.scss';
+import resources from './locales/index.js';
 import Stub from './components/Stub/Stub.jsx';
 
-        <Stub />
+const init = async () => {
+  const i18n = i18next.createInstance();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const mode = process.env.NODE_ENV || 'development';
+  const isDevelop = mode === 'development';
+
+  await i18n
+    .use(initReactI18next)
+    .init({
+      resources,
+      debug: isDevelop,
+      fallbackLng: 'ru',
+      interpolation: {
+        escapeValue: false,
+      },
+    });
+
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <I18nextProvider i18n={i18n}>
+        <Stub />
+      </I18nextProvider>
+    </React.StrictMode>
+  );
+};
+
+init();
---
+++ src/locales/ru.js
@@ -0,0 +1,9 @@
+export default {
+  Stub: {
+    testcooking: 'Cooking Trail Protection🍳',
+    vkGroup: 'Группа ВКонтакте',
+    links: {
+      vk: 'https://vk.com/trailprotection',
+    },
+  },
+};
